236e1c01f6c8ef34d34954f4ff4c67215bbc9fd1
n.galanova <natalia.galanova@gmail.com>
2018-06-02 22:25:40 +0700

    [*]: created .net project Tron.net

---
+++ Tron.net/Tron.net/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tron.net
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Я служу Пользователю.");
+			Console.ReadKey();
+		}
+	}
+}
